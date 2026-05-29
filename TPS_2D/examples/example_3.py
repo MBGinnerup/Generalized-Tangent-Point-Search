8bde8b7a303dcc7e505115e5d065291f2f386aff
MBGinnerup <marcusbregi@gmail.com>
2026-05-29 13:51:08 +0200

    Update files

---
+++ TPS_2D/examples/example_3.py
@@ -0,0 +1,35 @@
+#%%
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..","..")))
+from TPS_2D.maps.random_maps import random_map
+from TPS_2D.planners.tps import TPS
+from TPS_2D.planners.tps_bidirectional import TPS_Bidirectional
+from TPS_2D.planners.tps_theta import TPS_Theta
+from TPS_2D.planners.core.pre_processing import TPS_preprocess
+from TPS_2D.utils.plot_utils import visualize
+from TPS_2D.planners.core.pre_processing import graph_builder
+from TPS_2D.utils import path_queries as pq
+
+# %% Generate map
+size = 100
+map = random_map(size=size, dense=0.4, distribution=0.5, relative_size=1, small=(1,5), large=(10,20))
+
+# %% Pre-Processing
+pre = TPS_preprocess(map)
+
+# %% Planning and build graph
+graph = {}
+start = pq.random_start(map)
+goal = pq.random_goal(map, start, min_dist=75)
+path = TPS(map, start, goal, pre=pre, graph=graph, build_graph=True)
+
+# %% Visualize
+visualize(map, path=path, start=start, goal=goal, graph=graph)
+
+# %% Build larger graph:
+graph = graph_builder(map, pre, build_method=TPS, min_dist=75, iterations=1000, existing_graph=graph)
+
+# %% Visualize
+visualize(map, graph=graph)
+# %%
--- TPS_2D/utils/plot_utils.py
+++ TPS_2D/utils/plot_utils.py
@@ -3,13 +3,12 @@ import numpy as np
 from matplotlib.colors import ListedColormap
 import networkx as nx
 
-def visualize(occupancy_map, path=None, start=None, goal=None, fig_size=6, linewidth=1.5, markersize=3,s=40):
+def visualize(occupancy_map, path=None, start=None, goal=None, graph=None, fig_size=6, linewidth=1.5, markersize=3, s=20):
     """
-    Visualize a 2D occupancy grid and a path.
+    Visualize a 2D occupancy grid, path, and graph structure.
 
     The function plots occupied cells as a binary map and optionally
-    overlays a path together with start and goal positions. A light grid
-    can also be displayed to improve spatial perception.
+    overlays a path together with start and goal positions. Addiotnally a graph can be plotted.
 
     Parameters:
         occupancy_map : ndarray
@@ -20,10 +19,10 @@ def visualize(occupancy_map, path=None, start=None, goal=None, fig_size=6, linew
             Start coordinate.
         goal : tuple, optional
             Goal coordinate.
+        graph : dict, optional
+            Graph structure to visualize.
         fig_size : int, optional
             Figure size in inches.
-        grid : bool, optional
-            If True, overlay grid lines.
         linewidth : float, optional
             Width of the plotted path.
         markersize : float, optional
@@ -32,29 +31,26 @@ def visualize(occupancy_map, path=None, start=None, goal=None, fig_size=6, linew
             Marker size for start and goal points.
     """
 
-    # -------------------------------------------------
-    # Prepare occupancy map
-    # -------------------------------------------------
     occupancy_map = occupancy_map.T
     occupancy_map = 1 - occupancy_map
 
     light_gray = (0.7, 0.7, 0.7)
 
-    cmap = ListedColormap([light_gray,(1, 1, 1)])
+    cmap = ListedColormap([light_gray, (1, 1, 1)])
 
     fig, ax = plt.subplots(figsize=(fig_size, fig_size))
 
     ax.imshow(occupancy_map, cmap=cmap, origin='lower')
 
-    nx, ny = occupancy_map.shape
+    nx_size, ny_size = occupancy_map.shape
 
-    for x in range(ny + 1):
+    for x in range(ny_size + 1):
 
-        ax.vlines(x - 0.5, -0.5, nx - 0.5, color='black', linewidth=0.5, alpha=0.05)
-            
-    for y in range(nx + 1):
+        ax.vlines(x - 0.5, -0.5, nx_size - 0.5, color='black', linewidth=0.5, alpha=0.05)
 
-        ax.hlines(y - 0.5, -0.5, ny - 0.5, color='black', linewidth=0.5, alpha=0.05)
+    for y in range(nx_size + 1):
+
+        ax.hlines(y - 0.5, -0.5, ny_size - 0.5, color='black', linewidth=0.5,alpha=0.05)
 
     if path is not None:
 
@@ -62,7 +58,7 @@ def visualize(occupancy_map, path=None, start=None, goal=None, fig_size=6, linew
 
         if len(path) > 1:
 
-            ax.plot(path[:, 0], path[:, 1], color='steelblue', marker='o', markersize=markersize, linewidth=linewidth)
+            ax.plot(path[:, 0], path[:, 1], marker='o', markersize=markersize, linewidth=linewidth)
 
     if start is not None:
 
@@ -72,5 +68,55 @@ def visualize(occupancy_map, path=None, start=None, goal=None, fig_size=6, linew
 
         ax.scatter(goal[0], goal[1], color='green', s=s, zorder=10)
 
-    plt.show()
+    if graph is not None:
 
+        fallback_paths = []
+
+        G = nx.Graph()
+
+        for (current, obs_goal), neighbors in graph.items():
+
+            if  isinstance(neighbors, set):
+
+                for tangent, distance in neighbors:
+
+                    if distance:
+                        G.add_edge(current, tangent)
+                        
+            else:
+                pth = neighbors
+                fallback_paths.append(pth)
+
+        pos = {}
+
+        for node in G.nodes:
+
+            if len(node) == 3:
+
+                pos[node] = (node[0], node[1])
+
+            else:
+
+                pos[node] = node
+
+        for u, v in G.edges:
+
+            x = [pos[u][0], pos[v][0]]
+            y = [pos[u][1], pos[v][1]]
+
+            ax.plot(x, y, color='slateblue', linewidth=0.7, alpha=0.6)
+
+        xs = [pos[n][0] for n in G.nodes]
+        ys = [pos[n][1] for n in G.nodes]
+
+        ax.scatter(xs, ys, color='slateblue', s=5, alpha=0.7)
+
+        for pth in fallback_paths:
+
+            pth = np.asarray(pth)
+
+            if len(pth) > 1:
+
+                ax.plot(pth[:, 0], pth[:, 1], color='slateblue', linewidth=0.7, alpha=0.6)
+
+    plt.show()
